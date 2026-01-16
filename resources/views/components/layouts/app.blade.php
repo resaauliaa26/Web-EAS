1d3934bec29022cc4156ced41a8d2a5d3c54217f
resaauliaa26 <resaamanda26@gmail.com>
2026-01-16 20:59:16 +0700

    🔥🔥🔥 FINAL FIX: Remove Dockerfile, fix railway.toml, CSS-only

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- ✅ JS TETAP VIA VITE -->
-    @vite(['resources/js/app.js'])
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- External Libraries -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
